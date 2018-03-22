3d9246806ce98b66ee14736919cbdf2b31cd84cf
ishiiprints <adam.jacques@missguided.com>
2018-03-22 11:48:05 +0000

    :v:

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var autoprefixerOptions = {
+  browsers: ['last 5 versions', '> 10%']
+};
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/style.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', function() {
+    browserSync.init({
+      port: 7184,
+      //  https: true,
+      ghostMode: false,
+      server: {
+          baseDir: "./"
+      }
+    });
+    gulp.watch('./js/**/*.js').on("change", browserSync.reload);
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./css/**/*.css').on("change", browserSync.reload);
+    gulp.watch('**/*.html').on("change", browserSync.reload);
+});
