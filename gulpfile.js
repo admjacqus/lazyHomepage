1776d86c8e6076b875050314f4f891481d109abc
ishiiprints <adam.jacques@missguided.com>
2018-04-10 14:55:34 +0100

    :v: Jump and bottom row lazyness fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 var autoprefixerOptions = {
-  browsers: ['last 5 versions', '> 10%']
+  browsers: ["last 5 versions", "> 5%"]
 };
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/style.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }).on('error', sass.logError))
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      }).on("error", sass.logError)
+    )
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('default', function() {
-    browserSync.init({
-      port: 7184,
-      //  https: true,
-      ghostMode: false,
-      server: {
-          baseDir: "./"
-      }
-    });
-    gulp.watch('./js/**/*.js').on("change", browserSync.reload);
-    gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch('./css/**/*.css').on("change", browserSync.reload);
-    gulp.watch('**/*.html').on("change", browserSync.reload);
+gulp.task("default", function() {
+  browserSync.init({
+    port: 7184,
+    //  https: true,
+    ghostMode: false,
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+  gulp.watch("./sass/**/*.scss", ["sass"]);
+  gulp.watch("./css/**/*.css").on("change", browserSync.reload);
+  gulp.watch("**/*.html").on("change", browserSync.reload);
 });
